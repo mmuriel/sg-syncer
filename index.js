6d96db89da8ebca2e63f807f89502fee0cc5a07f
Mauricio Muriel <mauricio.muriel@calitek.net>
2016-08-09 15:22:44 -0500

    Definiendo el archivo de inicio del proyecto y ajustes de rutas en el SgCliente

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-var SgServer = require('./lib/SgServer.js');
+var SgCnf = require('./lib/SgCnf.js');
+if (SgCnf.profile === 'server'){
+	var SgServer = require('./lib/SgServer.js');
+	console.log(SgServer);
+}
+else if (SgCnf.profile === 'client'){
+	var SgCliente = require('./lib/SgCliente.js');
+	console.log(SgCliente);
+}
 
-console.log(SgServer);
